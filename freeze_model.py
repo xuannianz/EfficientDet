8c43d7e61e6a64d89570df7d27819e8b2884a851
dgkim <km23630@gmail.com>
2020-02-21 15:38:16 +0900

    optimize inference_frozen_graph.py, preprocess_image()

--- freeze_model.py
+++ freeze_model.py
@@ -23,22 +23,28 @@ def freeze_session(session, keep_var_names=None, output_names=None, clear_device
             session, input_graph_def, output_names, freeze_var_names)
         return frozen_graph
 
-phi = 1
-weighted_bifpn = False
-model_path = 'checkpoints/2019-12-03/pascal_05_0.6283_1.1975_0.8029.h5'
-image_sizes = (512, 640, 768, 896, 1024, 1280, 1408)
-image_size = image_sizes[phi]
-classes = [
-    'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair',
-    'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor',
-]
-num_classes = len(classes)
-score_threshold = 0.5
-model, prediction_model = efficientdet(phi=phi,
-                                       weighted_bifpn=weighted_bifpn,
-                                       num_classes=num_classes,
-                                       score_threshold=score_threshold)
-prediction_model.load_weights(model_path, by_name=True)
 
-frozen_graph = freeze_session(K.get_session(),  output_names=[out.op.name for out in prediction_model.outputs])
-tf.train.write_graph(frozen_graph, "./checkpoints/2019-12-03/", "pascal_05.pb", as_text=False)
+def main():
+    phi = 1
+    weighted_bifpn = False
+    model_path = 'checkpoints/2019-12-03/pascal_05_0.6283_1.1975_0.8029.h5'
+    image_sizes = (512, 640, 768, 896, 1024, 1280, 1408)
+    image_size = image_sizes[phi]
+    classes = [
+        'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair',
+        'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor',
+    ]
+    num_classes = len(classes)
+    score_threshold = 0.5
+    model, prediction_model = efficientdet(phi=phi,
+                                           weighted_bifpn=weighted_bifpn,
+                                           num_classes=num_classes,
+                                           score_threshold=score_threshold)
+    prediction_model.load_weights(model_path, by_name=True)
+    
+    frozen_graph = freeze_session(K.get_session(),  output_names=[out.op.name for out in prediction_model.outputs])
+    tf.train.write_graph(frozen_graph, "./checkpoints/2019-12-03/", "pascal_05.pb", as_text=False)
+
+
+if __name__ == '__main__':
+    main()
